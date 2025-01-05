2821f44a97577b915924a09e0c442bc8eda0b755
yd072 <luiquirong@gmail.com>
2025-01-05 21:39:12 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -782,15 +782,19 @@ for (let i = 0; i < 256; ++i) {
  * @returns {string} UUID 字符串
  */
 function unsafeStringify(arr, offset = 0) {
-	// 直接从查找表中获取每个字节的十六进制表示，并拼接成 UUID 格式
-	// 8-4-4-4-12 的分组是通过精心放置的连字符 "-" 实现的
-	// toLowerCase() 确保整个 UUID 是小写的
-	return (byteToHex[arr[offset + 0]] + byteToHex[arr[offset + 1]] + byteToHex[arr[offset + 2]] + byteToHex[arr[offset + 3]] + "-" +
-		byteToHex[arr[offset + 4]] + byteToHex[arr[offset + 5]] + "-" +
-		byteToHex[arr[offset + 6]] + byteToHex[arr[offset + 7]] + "-" +
-		byteToHex[arr[offset + 8]] + byteToHex[arr[offset + 9]] + "-" +
-		byteToHex[arr[offset + 10]] + byteToHex[arr[offset + 11]] + byteToHex[arr[offset + 12]] +
-		byteToHex[arr[offset + 13]] + byteToHex[arr[offset + 14]] + byteToHex[arr[offset + 15]]).toLowerCase();
+	// 使用数组拼接代替 + 拼接，提高效率
+	return [
+		byteToHex[arr[offset + 0]], byteToHex[arr[offset + 1]], byteToHex[arr[offset + 2]], byteToHex[arr[offset + 3]],
+		'-',
+		byteToHex[arr[offset + 4]], byteToHex[arr[offset + 5]],
+		'-',
+		byteToHex[arr[offset + 6]], byteToHex[arr[offset + 7]],
+		'-',
+		byteToHex[arr[offset + 8]], byteToHex[arr[offset + 9]],
+		'-',
+		byteToHex[arr[offset + 10]], byteToHex[arr[offset + 11]], byteToHex[arr[offset + 12]], byteToHex[arr[offset + 13]], 
+		byteToHex[arr[offset + 14]], byteToHex[arr[offset + 15]]
+	].join('').toLowerCase();
 }
 
 /**
