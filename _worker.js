19696bd2d5a5e3dd3e4d8bdb1e6691e9872622d4
yd072 <luiquirong@gmail.com>
2025-01-07 09:44:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -677,7 +677,7 @@ function safeCloseWebSocket(socket) {
 
 // 预计算 0-255 每个字节的十六进制表示
 const byteToHex = Array.from({ length: 256 }, (_, i) => (i + 256).toString(16).slice(1));
-}
+
 
 /**
  * 快速地将字节数组转换为 UUID 字符串，不进行有效性检查
