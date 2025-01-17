315efe7b31b8e1835583f3a0d9be37788f640f57
yd072 <luiquirong@gmail.com>
2025-01-17 22:25:27 +0800

    Update 源码.js

--- 源码.js
+++ 源码.js
@@ -2011,32 +2011,3 @@ async function 处理地址列表(地址列表) {
 	
 	return 分类地址;
 }
-
-// 添加请求工具类
-class RequestUtil {
-    static async fetchWithTimeout(url, options = {}, timeout = 5000) {
-        const controller = new AbortController();
-        const timeoutId = setTimeout(() => controller.abort(), timeout);
-        
-        try {
-            const response = await fetch(url, {
-                ...options,
-                signal: controller.signal
-            });
-            return response;
-        } finally {
-            clearTimeout(timeoutId);
-        }
-    }
-
-    static async fetchWithRetry(url, options = {}, retries = 3) {
-        for (let i = 0; i < retries; i++) {
-            try {
-                return await this.fetchWithTimeout(url, options);
-            } catch (error) {
-                if (i === retries - 1) throw error;
-                await new Promise(resolve => setTimeout(resolve, 1000 * Math.pow(2, i)));
-            }
-        }
-    }
-}
