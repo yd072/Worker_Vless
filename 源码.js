5eab822eb5c81fd6811eeb7c300d3dfa080ffb6e
yd072 <luiquirong@gmail.com>
2025-01-21 14:49:18 +0800

    Update 源码.js

--- 源码.js
+++ 源码.js
@@ -12,7 +12,7 @@ let parsedSocks5Address = {};
 let enableSocks = false;
 
 let noTLS = 'false';
-const expire = 4102329600;//2099-12-31
+const expire = -1;//9999
 let proxyIPs;
 let socks5s;
 let go2Socks5s = [
